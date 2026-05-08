89421d2fc2dec8db16a7be4dbc9114f5522caa17
MD.Neyamotullah Bhuiyan (Fardin) <fardinahmedfardin1@gmail.com>
2026-05-08 12:39:52 +0600

    Create Write a program to calculate the area of a circle.cpp

---
+++ 7. Variables and Data Types/Write a program to calculate the area of a circle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float r;
+    cout<<"Radius : ";
+    cin>>r;
+    
+    cout<<"Area of Circle : "<<3.14*r*r<<endl;
+    
+    return 0;
+}
