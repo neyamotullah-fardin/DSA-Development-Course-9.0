19ceedef23a236658a1f4d8b60096dbb32370461
MD.Neyamotullah Bhuiyan (Fardin) <fardinahmedfardin1@gmail.com>
2026-05-08 12:39:35 +0600

    Create In a program,input the side of a square. You have to output the area of the square.cpp

---
+++ 7. Variables and Data Types/In a program,input the side of a square. You have to output the area of the square.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int side;
+    cout<<"Write the value of side : ";
+    cin>>side;
+    cout<<"The area of the square is "<<side * side;
+    
+    return 0;
+}
